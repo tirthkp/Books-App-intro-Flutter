fcd234938ed32997d86c2221a1ef23e30145c355
tirthkp <tirthp57@gmail.com>
2022-06-28 01:34:11 +0530

    added 3 pages

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,142 +1,88 @@
+import 'package:books_app/pages/page1.dart';
+import 'package:books_app/pages/page2.dart';
+import 'package:books_app/pages/page3.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class IntroPage extends StatelessWidget {
+class IntroPage extends StatefulWidget {
+  const IntroPage({Key? key}) : super(key: key);
+
+  @override
+  State<IntroPage> createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
   final _controller = PageController();
-  IntroPage({Key? key}) : super(key: key);
+  late int pageChanged;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.teal,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'OUR',
-                      style: GoogleFonts.robotoMono(
-                        letterSpacing: 2,
-                        decoration: TextDecoration.lineThrough,
-                        fontSize: 20,
-                        color: Colors.white70,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      'YOUR BOOKS',
-                      style: GoogleFonts.robotoMono(
-                        letterSpacing: 2,
-                        fontSize: 20,
-                        color: Colors.white70,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Become the best ',
-                    style: GoogleFonts.robotoMono(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    'in your',
-                    style: GoogleFonts.robotoMono(
-                      color: Colors.white70,
-                      fontSize: 20,
-                      // fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-              Center(
-                child: Text(
-                  'industry,One class at a time ',
-                  style: GoogleFonts.robotoMono(
-                    color: Colors.white70,
-                    fontSize: 22,
+        child: Stack(
+          children: [
+            PageView(
+              controller: _controller,
+              scrollDirection: Axis.horizontal,
+              onPageChanged: (index) {
+                setState(() {
+                  pageChanged = index;
+                });
+              },
+              children: const [
+                Page1(),
+                Page2(),
+                Page3(),
+              ],
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 250),
+                child: SmoothPageIndicator(
+                  onDotClicked: (index) {
+                    _controller.animateToPage(index,
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.ease);
+                  },
+                  controller: _controller,
+                  count: 3,
+                  effect: const WormEffect(
+                    dotColor: Colors.white30,
+                    activeDotColor: Colors.white,
+                    dotHeight: 5,
+                    dotWidth: 25,
+                    type: WormType.thin,
+                    spacing: 12,
                   ),
                 ),
               ),
-              const SizedBox(
-                height: 25,
-              ),
-              Text(
-                'Learn new professions from the',
-                style: GoogleFonts.robotoMono(
-                  color: Colors.white70,
-                  fontSize: 18,
-                ),
-              ),
-              Text(
-                'comfort of your home or anywhere',
-                style: GoogleFonts.robotoMono(
-                  color: Colors.white70,
-                  fontSize: 18,
-                ),
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              SmoothPageIndicator(
-                controller: _controller,
-                count: 3,
-                effect: const WormEffect(
-                  dotColor: Colors.white30,
-                  activeDotColor: Colors.white,
-                  dotHeight: 5,
-                  dotWidth: 25,
-                  type: WormType.thin,
-                  spacing: 12,
-                ),
-              ),
-              Image.asset(
-                'images/1.png',
-                height: 450,
-                width: double.infinity,
-                fit: BoxFit.fitHeight,
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
       floatingActionButton: Padding(
         padding: const EdgeInsets.only(bottom: 20.0),
-        child: Container(
-          height: 50,
-          width: 50,
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            shape: BoxShape.circle,
-            boxShadow: [
-              BoxShadow(
-                color: Color.fromARGB(255, 33, 255, 203),
-                blurRadius: 15,
-                spreadRadius: 5,
-              ),
-            ],
-          ),
-          child: const Icon(
-            Icons.arrow_forward_ios_outlined,
-            color: Colors.teal,
+        child: InkWell(
+          onTap: () {},
+          child: Container(
+            height: 50,
+            width: 50,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                  color: Color.fromARGB(255, 33, 255, 203),
+                  blurRadius: 15,
+                  spreadRadius: 5,
+                ),
+              ],
+            ),
+            child: const Icon(
+              Icons.arrow_forward_ios_outlined,
+              color: Colors.teal,
+            ),
           ),
         ),
       ),
