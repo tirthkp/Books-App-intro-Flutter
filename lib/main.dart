c0576abdc2d291d0428488872756d3ec0302ed63
tirthkp <tirthp57@gmail.com>
2022-07-08 21:09:34 +0530

    added title

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Books',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.teal,
